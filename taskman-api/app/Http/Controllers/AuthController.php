7d1a0ad2c8b6d5896010d76edae690dfcfa4810c
raisa-SSTL <raisa@synergysailtech.com>
2024-12-29 01:22:10 +0600

    added jwt token

---
+++ taskman-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password'])
+        ]);
+        $token = auth('api')->login($user);
+        return $this->respondWithToken($token);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- taskman-api/routes/api.php
+++ taskman-api/routes/api.php
@@ -3,10 +3,35 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group([], function ($router) {
+    Route::post('login', [AuthController::class,'login']);
+    Route::post('register', [AuthController::class,'register']);
+});
+
+Route::middleware(['auth:api'])->group(function(){
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('me', [AuthController::class,'me']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+
+    // T A S K
+
+    // Route::post('/task', [TaskController::class, 'store']);
+    // Route::get('/task-list', [TaskController::class, 'index']);
+    // Route::get('show-task-details/{id}', [TaskController::class, "show"]);
+    // Route::post('/update-task/{id}', [TaskController::class, 'update']);
+    // Route::post('/delete-task/{id}', [TaskController::class, 'destroy']);
+    // Route::post('/search-task', [TaskController::class, 'search']);
+    // Route::post('/find-filtered-tasks', [TaskController::class, 'filteredTasks']);
+    // Route::post('/year-wise-tasks', [TaskController::class, 'yearWiseTasks']);
+    // Route::post('/month-year-completed-tasks', [TaskController::class, 'monthYearCompletedTasks']);
+
+});
 
 // T A S K
 
@@ -19,3 +44,5 @@ Route::post('/search-task', [TaskController::class, 'search']);
 Route::post('/find-filtered-tasks', [TaskController::class, 'filteredTasks']);
 Route::post('/year-wise-tasks', [TaskController::class, 'yearWiseTasks']);
 Route::post('/month-year-completed-tasks', [TaskController::class, 'monthYearCompletedTasks']);
+
+
