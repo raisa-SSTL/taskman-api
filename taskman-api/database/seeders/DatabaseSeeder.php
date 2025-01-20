5cae2a58319478825c07f8322c4875cbb6802a89
raisa-SSTL <raisa@synergysailtech.com>
2025-01-21 03:05:36 +0600

    resolved errors in db seeder

--- taskman-api/database/seeders/DatabaseSeeder.php
+++ taskman-api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Employee;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
